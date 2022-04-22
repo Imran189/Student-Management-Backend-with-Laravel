73af758882b49ce1f334f7df1d93cff55d950b87
Imran189 <imranimu189@gmail.com>
2022-04-22 10:21:28 +0600

    password reset

--- app/Http/Controllers/Api/Auth/ForgetPasswordController.php
+++ app/Http/Controllers/Api/Auth/ForgetPasswordController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 class ForgetPasswordController extends Controller
 {
-    //
+    public function forgot(Request $request){
+        $validate= validator::make($request->all(),[
+            'email'=>'required|email|exists:users',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'errors' => $validate->errors()
+            ],422);
+        }
+
+        $email = $request->email;
+        $token = Str::random(6);
+        DB::table('password_resets')->insert([
+            'email'=>$email,
+            'token'=>$token,
+            'created_at'=> now()->addHours(1)
+        ]);
+
+        //mail send
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\ForgetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is ase "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::prefix('v1')->group(function(){
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/forgot', [ForgetPasswordController::class, 'forgot']);
 
     Route::get('/login',function(){
         return response()->json([
